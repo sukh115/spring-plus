ab74983b8370d48bdd07e0f1c5b2ccd29d8a2516
sukh115 <sukh1151102@gmail.com>
2025-05-01 10:00:58 +0900

    fix : Level.1-3

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("    SELECT t\n" +
+            "    FROM Todo t\n" +
+            "    LEFT JOIN FETCH t.user u\n" +
+            "    WHERE (:weather IS NULL OR t.weather = :weather)\n" +
+            "      AND (:start IS NULL OR t.modifiedAt >= :start)\n" +
+            "      AND (:end IS NULL OR t.modifiedAt <= :end)\n" +
+            "    ORDER BY t.modifiedAt DESC")
+    Page<Todo> searchByWeatherAndDateRange(@Param("weather") String weather, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end, Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
