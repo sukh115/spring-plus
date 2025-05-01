7ed23fba78658c8a9995108f7d6d552c0d6fca61
sukh115 <sukh1151102@gmail.com>
2025-05-02 08:59:17 +0900

    fix : 헬스체크 로그추가

--- src/main/java/org/example/expert/domain/healthcheck/HealthCheckController.java
+++ src/main/java/org/example/expert/domain/healthcheck/HealthCheckController.java
@@ -1,14 +1,16 @@
 package org.example.expert.domain.healthcheck;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthCheckController {
 
-    @GetMapping("/health")
     public ResponseEntity<String> health() {
+        log.info("✅ Health check");
         return ResponseEntity.ok("OK");
     }
 }
