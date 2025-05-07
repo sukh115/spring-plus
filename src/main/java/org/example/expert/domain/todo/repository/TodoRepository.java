778a25429d3e2a58f9e1f09bfe0d9bf07e2123d2
sukh115 <sukh1151102@gmail.com>
2025-05-07 15:48:35 +0900

    fix : 패치조인 문제 해결

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,9 +13,9 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long>,TodoQueryRepository {
 
+    @EntityGraph(attributePaths = {"user"})
     @Query("    SELECT t\n" +
             "    FROM Todo t\n" +
-            "    LEFT JOIN FETCH t.user u\n" +
             "    WHERE (:weather IS NULL OR t.weather = :weather)\n" +
             "      AND (:start IS NULL OR t.modifiedAt >= :start)\n" +
             "      AND (:end IS NULL OR t.modifiedAt <= :end)\n" +
